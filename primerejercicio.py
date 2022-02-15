7b5f2a5cac29e6f934036f1ca65f219d6a125a78
Mariel Luna <44310879+OeMiku@users.noreply.github.com>
2022-02-14 23:07:33 -0600

    Katas subidas
    He subido las katas correspondientes

---
+++ primerejercicio.py
@@ -0,0 +1,7 @@
+from datetime import date
+date.today()
+print(date.today())
+print("Today's date is: " + str(date.today()))
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
